5a16f69170ffd7c7808d66d568d813ce25db9966
PranavThor <156145145+PranavThorali@users.noreply.github.com>
2025-06-09 19:07:22 -0400

    Add files via upload
    MySQL files

---
+++ End-to-End Projects/Demand&Availability Dataset/pject01.sql
@@ -0,0 +1,9 @@
+use prod;
+
+SELECT * FROM prod.`prod+env+inventory+dataset`;
+
+update prod.`prod+env+inventory+dataset`
+set `Product ID` = 7 where `Product ID` = 21;
+
+update prod.`prod+env+inventory+dataset`
+set `Product ID` = 11 where `Product ID` =22;
